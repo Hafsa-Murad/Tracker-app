749e78e04319d38ba69ba9c0e963d8885825983d
Hafsa-Murad <hafsamurad2022@gmail.com>
2024-08-06 03:15:29 +0500

    first inventary

--- app/components/SearchProduct.js
+++ app/components/SearchProduct.js
@@ -40,14 +40,14 @@ export default function SearchProduct({ stockData, setStockData }) {
         placeholder="Search..."
         value={searchQuery}
         onChange={handleSearch}
-        className="border p-2 rounded w-full sm:w-48 mb-4 text-lg w-full sm:w-3/4" // Increase text size in input field
+        className="border p-2 rounded w-full sm:w-48 mb-4 text-lg lg:w-1/2 xl:w-1/3 " // Increase text size in input field
       />
       {searchResults.length > 0 && (
-        <div className="border rounded w-full sm:w-48 bg-white w-full sm:w-3/4">
+        <div className="border rounded w-full sm:w-48 bg-white lg:w-1/2 xl:w-1/3 ">
           {searchResults.map((result) => (
             <div key={result.id} className="p-4 border-b last:border-b-0 flex items-center justify-between"> {/* Increased padding */}
               <span className="text-lg">{result.name}</span> {/* Increased font size */}
-              <span className="text-lg">Quantity: {result.quantity}</span> {/* Increased font size */}
+              <span>Quantity: {result.quantity}</span> {/* Increased font size */}
               <div className="flex items-center gap-4"> {/* Increased gap between buttons */}
                 <button onClick={() => handleDecrease(result.id)} className="bg-red-500 text-white px-3 py-1 rounded text-lg">-</button> {/* Increased button size */}
                 <button onClick={() => handleIncrease(result.id)} className="bg-green-500 text-white px-3 py-1 rounded text-lg">+</button> {/* Increased button size */}
